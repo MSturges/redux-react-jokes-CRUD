9d96e465fff48e716a4c7648ff59a2fa4182af4c
MSturges <Sturges3232@gmail.com>
2016-11-06 16:34:01 -0700

    MVP COMPLETE MF

--- src/components/joke_edit.js
+++ src/components/joke_edit.js
@@ -3,16 +3,19 @@ import { reduxForm, initialize } from 'redux-form';
 import { editJoke, fetchJoke } from '../actions/index';import { Link } from 'react-router';
 import axios from 'axios';
 
-class JokeEdit extends Component {
+class JokeEditt extends Component {
 
   static contextTypes = {
     router: PropTypes.object
   }
 
   componentWillMount() {
-    this.props.fetchBlog(this.props.params.id)
-  }
+    this.props.fetchJoke(this.props.params.id)
+    .then(()=> {
+      console.log(this.state);
+    })
 
+  }
 
   onSubmit(formData) {
     console.log('Edit form data...', formData);
@@ -24,59 +27,49 @@ class JokeEdit extends Component {
 
 
   render() {
-    const { fields: { author, topic, content, image_url }, handleSubmit } = this.props;
-    if(!blog) return (<div>Loading...</div>);
+    const { fields: { title, genre, author, joke } , handleSubmit } = this.props;
+
+    if(!joke) return (<div>Loading...</div>);
 
     return (
       <div>
 
-        <div>
-          <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="form blogItemContainer editForm">
-            <Link to="/" className="btn link">Home</Link>
-            <Link to={`/blogs/${this.props.params.id}`} className="btn link">Back</Link>
-            <button
-              onClick={this.onDeleteClick.bind(this)}
-              className="btn formButtons">
-              Delete
-            </button>
-            <h1>Edit Writing</h1>
-            <div className={`form-group ${author.touched && author.invalid ? 'has-danger' : ''}`}>
-              <label>Author</label>
-              <div className="text-danger">
-                {author.touched ? author.error : null}
-              </div>
-              <input type="text" className="form-control" {...author} />
-            </div>
-            <div className={`form-group ${topic.touched && topic.invalid ? 'has-danger' : ''}`}>
-              <label>Topic</label>
-              <div className="text-danger">
-                {topic.touched ? topic.error : null}
-              </div>
-              <input type="text" className="form-control" {...topic} />
-            </div>
-            <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
-              <label>Content</label>
-              <div className="text-danger">
-                {content.touched ? content.error : null}
-              </div>
-              <textarea type="text" className="form-control" {...content} />
-            </div>
-            <div className={`form-group ${image_url.touched && image_url.invalid ? 'has-danger' : ''}`}>
-              <label>Image URL</label>
-              <div className="text-danger">
-                {image_url.touched ? image_url.error : null}
-              </div>
-              <input type="text" className="form-control" {...image_url} />
-            </div>
-            <button type="submit" className="btn formButtons">Submit</button>
-            <Link to="/" className="btn formButtons">Cancel</Link>
-          </form>
-        </div>
+      <div>
+      <form className="animated bounceInDown" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+      <h3>Create A New Post</h3>
+      <div className={ `form-group ${title.touched && title.invalid ? 'has-danger': ''}`}>
+      <label>Title</label>
+      <input type="text" className="form-control" {...title}/>
+      <div className='text-help form-control-label'>{title.touched ? title.error : ''}</div>
+      </div>
+
+      <div className={ `form-group ${genre.touched && genre.invalid ? 'has-danger': ''}`}>
+      <label>Genre</label>
+      <input type="text" className="form-control" {...genre}/>
+      <div className='text-help form-control-label'>{genre.touched ? genre.error : ''}</div>
+      </div>
+
+      <div className={ `form-group ${author.touched && author.invalid ? 'has-danger': ''}`}>
+      <label>Author</label>
+      <input type="text" className="form-control" {...author}/>
+      <div className='text-help form-control-label'>{author.touched ? author.error : ''}</div>
+      </div>
+
+      <div className={ `form-group ${joke.touched && joke.invalid ? 'has-danger': ''}`}>
+      <label>Content</label>
+      <textarea type="text" className="form-control" {...joke}/>
+      <div className='text-help form-control-label'>{joke.touched ? joke.error : ''}</div>
+      </div>
+
+      <Link to="/" className="btn btn-danger">Back</Link>
+      <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
+      </div>
       </div>
     )
   }
 }
-
 
 function validate(values) {
   const errors = {};
@@ -97,18 +90,18 @@ function validate(values) {
 }
 
 export default reduxForm({
-  form: 'BlogsNewForm',
-  fields: ['author', 'topic', 'content', 'image_url'],
+  form: 'PostNewForm',
+  fields: ['title', 'genre', 'author', 'joke'],
   validate: validate
 }, (state) => {
-  if(state.blogs.blog){
+  if(state.jokes.joke){
     return {
       initialValues: {
-        author: state.blogs.blog.author,
-        topic: state.blogs.blog.topic,
-        content: state.blogs.blog.content,
-        image_url: state.blogs.blog.image_url
+        title: state.jokes.joke.title,
+        genre: state.jokes.joke.genre,
+        author: state.jokes.joke.author,
+        joke: state.jokes.joke.joke
       },
     };
   }
-}, { editJoke, fetchJoke })(JokeEdit);
+}, { editJoke, fetchJoke })(JokeEditt);
--- src/components/joke_show.js
+++ src/components/joke_show.js
@@ -27,29 +27,29 @@ class JokeShow extends Component {
 
     return (
       <div className="joke-show animated bounceInLeft row" key={joke.id}>
-<div className="col-md-2 col-lg-2 joke-show-item-container ">
+      <div className="col-md-2 col-lg-2 joke-show-item-container ">
 
-<h3 className="firstH3"><span className="">Title</span></h3>
-<h4 className="joke-show-item">{joke.title}</h4>
+      <h3 className="firstH3"><span className="">Title</span></h3>
+      <h4 className="joke-show-item">{joke.title}</h4>
+      
+      <h3><span className="">Genre</span></h3>
+      <h4 className="joke-show-item">{joke.genre}</h4>
 
-<h3><span className="">Genre</span></h3>
-<h4 className="joke-show-item">{joke.genre}</h4>
+      <h3><span className="">Author</span></h3>
+      <h4 className="joke-show-item">{joke.author}</h4>
 
-<h3><span className="">Author</span></h3>
-<h4 className="joke-show-item">{joke.author}</h4>
-
-</div>
-<p className="col-md-8 col-sm-8 col-xs-8 joke-show-joke">{joke.joke}</p>
-<div className="col-md-2 joke-show-edit">
-<Link to="/">Back To Jokes</Link>
-<Link to="/joke/edit/:id">Edit</Link>
-<button
-onClick={this.onDeleteClick.bind(this)}
-className="btn btn-danger pull-xs-right">
-Delete Post
-</button>
-</div>
-</div>
+      </div>
+      <p className="col-md-8 col-sm-8 col-xs-8 joke-show-joke">{joke.joke}</p>
+      <div className="col-md-2 joke-show-edit">
+      <Link to="/">Back To Jokes</Link>
+      <Link to="/joke/edit/:id">Edit</Link>
+      <button
+      onClick={this.onDeleteClick.bind(this)}
+      className="btn btn-danger pull-xs-right">
+      Delete Post
+      </button>
+      </div>
+      </div>
 
 
 
