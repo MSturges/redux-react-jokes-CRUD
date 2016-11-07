f243fece0a3d99fc3f1c686906b129167c3d6f7d
MSturges <Sturges3232@gmail.com>
2016-11-07 14:30:07 -0700

    ready for deployment

--- src/components/jokes_new.js
+++ src/components/jokes_new.js
@@ -27,25 +27,25 @@ class PostNew extends Component {
         <div className={ `form-group ${title.touched && title.invalid ? 'has-danger': ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
-          <div className='text-help form-control-label'>{title.touched ? title.error : ''}</div>
+          <div className='text-danger'>{title.touched ? title.error : ''}</div>
         </div>
 
         <div className={ `form-group ${genre.touched && genre.invalid ? 'has-danger': ''}`}>
           <label>Genre</label>
           <input type="text" className="form-control" {...genre}/>
-          <div className='text-help form-control-label'>{genre.touched ? genre.error : ''}</div>
+          <div className='text-danger'>{genre.touched ? genre.error : ''}</div>
         </div>
 
         <div className={ `form-group ${author.touched && author.invalid ? 'has-danger': ''}`}>
           <label>Author</label>
           <input type="text" className="form-control" {...author}/>
-          <div className='text-help form-control-label'>{author.touched ? author.error : ''}</div>
+          <div className='text-danger'>{author.touched ? author.error : ''}</div>
         </div>
 
         <div className={ `form-group ${joke.touched && joke.invalid ? 'has-danger': ''}`}>
           <label>Content</label>
           <textarea type="text" className="form-control" {...joke}/>
-          <div className='text-help form-control-label'>{joke.touched ? joke.error : ''}</div>
+          <div className='text-danger'>{joke.touched ? joke.error : ''}</div>
         </div>
 
         <div className="submit-btn-container">
@@ -67,15 +67,23 @@ function validate(values) {
   if (!values.title) {
     errors.title = 'Enter a Title';
   }
+  if (values.title && values.title.length > 20) errors.title = 'Cannot exceed 20 characters';
+
   if (!values.genre) {
     errors.genre = 'Enter a Genre';
   }
+  if (values.genre && values.genre.length > 20) errors.genre = 'Cannot exceed 20 characters';
+
   if (!values.author) {
     errors.author = 'Enter an Author';
   }
+  if (values.author && values.author.length > 20) errors.author = 'Cannot exceed 20 characters';
+
   if (!values.joke) {
     errors.joke = 'Enter some Content';
   }
+  if (values.joke && values.joke.length > 900) errors.joke = 'Cannot exceed 900 characters';
+
   return errors;
 }
 
